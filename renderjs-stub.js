653c072f9b3e6f70f863d1456175d717e2298f8b
Caleb James DeLisle <cjd@hyperboria.ca>
2013-11-15 18:51:49 +0100

    Updated renderjs-stub

--- renderjs-stub.js
+++ renderjs-stub.js
@@ -1,5 +1,17 @@
 /* -*- Mode: js; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/*! renderjs-stub v0.1  */
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ */
 /*global window, document */
 ;(function (window, document) {
   "use strict";
